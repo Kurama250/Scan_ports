0b5e4a445bd1dd0b9d71191dbd77838b7303bd33
Kurama <60360593+Kurama250@users.noreply.github.com>
2023-08-09 13:56:01 +0200

    Update package.bat

--- package.bat
+++ package.bat
@@ -1,17 +1,26 @@
 @echo off
 
-REM Vérifier si Python est installé
 python --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Python is not installed on your system.
+    echo Python is not installed on your system. Installing Python...
+
     exit /b 1
 )
 
-REM Installer les modules Python
-python -m pip install tqdm
-
-if %errorlevel% equ 0 (
-    echo Installation completed successfully !
+python -m pip show tqdm >nul 2>&1
+if %errorlevel% neq 0 (
+    echo tqdm module is not installed. Installing tqdm...
+    python -m pip install tqdm
+    if %errorlevel% equ 0 (
+        echo Installation of tqdm completed successfully!
+    ) else (
+        echo An error occurred while installing the tqdm module.
+    )
 ) else (
-    echo An error occurred while installing Python modules.
+    echo tqdm module is already installed.
 )
+
+echo All necessary components are installed.
+echo You can now proceed with your tasks.
+
+pause
